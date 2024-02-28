d3aac9e0289d8bba8845274c5c6332e44ed91f31
Jirawat Chaichompu <poompim083@gmail.com>
2024-02-28 13:50:10 +0700

    sss

--- main_test.go
+++ main_test.go
@@ -9,7 +9,6 @@ func TestHelloWorld(t *testing.T) {
     expected := "Hello, World!"
     result := "Hello, World!"
 
-    
     if result != expected {
         t.Errorf("Expected %s, but got %s", expected, result)
     }
