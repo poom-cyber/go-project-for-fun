8bb148fa176993ea3bd546e78ee7c574ff1b6f38
Jirawat Chaichompu <88946683+nwps00@users.noreply.github.com>
2024-02-28 13:45:38 +0700

    test

--- main_test.go
+++ main_test.go
@@ -8,7 +8,6 @@ import (
 func TestHelloWorld(t *testing.T) {
     expected := "Hello, World!"
     result := "Hello, World!"
-
     if result != expected {
         t.Errorf("Expected %s, but got %s", expected, result)
     }
