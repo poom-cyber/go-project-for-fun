d068275a09221d4cdb28671ad5fae914db1f046d
Jirawat Chaichompu <poompim083@gmail.com>
2024-02-28 14:39:08 +0700

    Update main.go

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
     router.GET("/albums/:id", getAlbumByID)
     router.POST("/albums", postAlbums)
 
-    router.Run("localhost:8080")
+    router.Run("localhost:8090")
 }
 
 // getAlbums responds with the list of all albums as JSON.
